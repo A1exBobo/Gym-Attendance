3766fd2c4c5b0ef5802c38fd2dbedea52f9287a5
A1exBobo <bobocalexandru55@gmail.com>
2026-09-23 12:57:36 +0300

    feat:Am modificat functiile din DatabaseHandler.Acum accepta parametrii..

--- Backend/Classes/DatabaseHandler.py
+++ Backend/Classes/DatabaseHandler.py
@@ -7,26 +7,26 @@ class DatabaseHandler:
     def CreateTable(self):
             cursor = self.conn.cursor();
 
-            create_table = "CREATE TABLE GYMATTENDANCE(Data date CHECK(Data <= current_date) );"
+            create_table = "CREATE TABLE IF NOT EXISTS GYMATTENDANCE(Data date CHECK(Data <= current_date) );"
 
             cursor.execute(create_table);
 
             self.conn.commit();
     
 
-    def InsertDate(self):
+    def InsertDate(self,date):
             cursor = self.conn.cursor();
 
-            insert_query = "INSERT INTO GYMATTENDANCE VALUES(CURRENT_DATE);"
-
+            insert_query = f"INSERT INTO GYMATTENDANCE VALUES({date});"
+            print(insert_query)
             cursor.execute(insert_query);
 
             self.conn.commit();
 
-    def DeleteRow(self):
+    def DeleteRow(self,date):
             cursor = self.conn.cursor();
 
-            delete_query = "DELETE FROM GYMATTENDANCE WHERE Data = CURRENT_DATE;"
+            delete_query = f"DELETE FROM GYMATTENDANCE WHERE Data = ({date});"
 
             cursor.execute(delete_query);
 
@@ -40,8 +40,10 @@ class DatabaseHandler:
 
 #scurt test
 db = DatabaseHandler()
-db.InsertDate()
-db.DeleteRow()
+db.CreateTable()
+db.InsertDate(" '2006-11-22' ")         #YYYY-MM-DD format 
+db.InsertDate(" '2026-09-23' ")
+db.DeleteRow(" '2006-11-22' ")
 db.CloseDb()
 
 ## SOURCE: https://www.sqlitetutorial.net/sqlite-python/creating-database/
